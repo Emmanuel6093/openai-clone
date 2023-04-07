7dad30d32ef2036e1fc1eb4758bab15b208f37a1
Emmanuel6093 <elorenzo6093@gmail.com>
2023-04-06 21:54:34 -0400

    added loading dots, type animation, & random ID to script.js

--- client/script.js
+++ client/script.js
@@ -7,5 +7,26 @@ const chatContainer = document.querySelector('#chat_container');
 let loadInterval;
 
 function loader(element) {
-  '...'
+  element.textContent = '.'
+
+  loadInterval = setInterval (() => {
+    element.textContent += '.';
+
+    if (element.textContent === '....') {
+      element.textContent = '.';
+    }
+  }, 300)
+}
+
+function typeText (element, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if(index < text.length) {
+      element.innerHTML += text.chartAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20)
 }
