728ed6c4f8e1dad7feab33a36d3ddb443d5b89a9
Emmanuel6093 <elorenzo6093@gmail.com>
2023-04-06 22:07:51 -0400

    added chat stripe & icon function

--- client/script.js
+++ client/script.js
@@ -6,7 +6,7 @@ const chatContainer = document.querySelector('#chat_container');
 
 let loadInterval;
 
-// generates loading dots appear 1 by 1 
+// generates loading dots appear 1 by 1.
 function loader(element) {
   element.textContent = '.'
 
@@ -19,7 +19,7 @@ function loader(element) {
   }, 300)
 }
 
-// makes text appear in type animation 1 by 1
+// makes text appear in type animation 1 by 1.
 function typeText (element, text) {
   let index = 0;
 
@@ -33,11 +33,31 @@ function typeText (element, text) {
   }, 20)
 }
 
-// always generates random ID
+// always generates random ID.
 function generateUniqueId() {
   const timestamp = Date.now();
   const randomNumber = Math.random();
   const hexadecimalString = randomNumber.toString(16);
 
   return `id-${timestamp}-${hexadecimalString}`;
+}
+
+// changes message stripe based on who is AI & User, also displays icon on who is AI & User. 
+function chatStripe(isAi, value, uniqueId) {
+  return (
+    `
+    <div> class='wrapper ${isAi && 'ai'}'>
+      <div class='chat'>
+        <div class='profile'>
+        <img 
+          src='${isAi ? bot : user}'
+          alt='${isAi ? 'bot' : 'user'}'
+        />
+        </div>
+        <div class='message' id='${uniqueId}'>${value}</div>
+      </div>
+    </div>
+
+    `
+  )
 }
